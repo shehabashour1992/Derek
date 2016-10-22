1e7b927ea704732b20834ac78a18abcfc1b36d03
Shehab Ashour <shehabashour1992@gmail.com>
2016-10-22 02:33:57 +0200

    first commit on branch 2

--- app/src/main/java/co/example/samsung/derek/MainActivity.java
+++ app/src/main/java/co/example/samsung/derek/MainActivity.java
@@ -4,14 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String name="shehab mohamed ashour";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void setName(String newname) {
+        this.name = newname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
     public static void main(String[] args)
     {
 
     }
+
 }
